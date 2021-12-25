e9a7261692d9ccfdf0ba43cf06b8bd69e54a3a18
EETagent <vojta.jungmann@gmail.com>
2021-12-25 16:06:33 +0100

    Oprava konfigurace

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import solidPlugin from "vite-plugin-solid";
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    target: "safari 12",
+    target: "safari12",
     polyfillDynamicImport: false,
   },
 });
