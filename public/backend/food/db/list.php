ae72365436da7a6de18b604d4367e9dcaaad865b
EETagent <vojta.jungmann@gmail.com>
2022-02-05 12:13:51 +0100

    Autorizace na backendu

--- public/backend/food/db/list.php
+++ public/backend/food/db/list.php
@@ -4,7 +4,23 @@ require_once '../../secret.php';
 require_once 'init.php';
 
 try {
-    $db = init_db();
+    $db = init_db(write: true);
+
+    $php_input = file_get_contents('php://input');
+
+    if (strlen($php_input) <= 0 && !isValidJSON($php_input)) {
+        throw new Exception('Invalid JSON');
+    }
+
+    $API_REQUEST = json_decode($php_input);
+
+    if (!isset($API_REQUEST->{'secret'})) {
+        throw new Exception('Missing secret property');
+    }
+
+    if ($API_REQUEST->{'secret'} === FOOD_TOKEN_SECRET) {
+        throw new Exception('Invalid secret property');
+    }
 
     $result = $db->query('SELECT * FROM food');
 
