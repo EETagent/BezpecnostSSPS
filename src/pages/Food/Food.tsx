4c84e3fc3d7ae072c0c8a96874e626ff22c9412e
EETagent <vojta.jungmann@gmail.com>
2022-02-13 13:26:36 +0100

    Přidání odhadu cen k jídlu

--- src/pages/Food/Food.tsx
+++ src/pages/Food/Food.tsx
@@ -8,6 +8,7 @@ interface FoodInterface {
   name: string;
   image: string;
   categories: FoodCategories[];
+  priceEst?: number;
   subitems?: FoodInterface[];
 }
 
@@ -29,21 +30,25 @@ const FOODS: FoodInterface[] = [
         image:
           "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/5802fab5-fdce-468a-a830-43e8001f5a72/Derivates/c00dc34a-e73d-42f0-a86e-e2fd967d33fe.jpg",
         categories: [FoodCategories.ALL, FoodCategories.VEGETARIAN, FoodCategories.FISH],
+        priceEst: 100,
       },
       {
         name: "Pizza Hawaii",
         image:
           "https://fototapeta12.cz/themes/default-bootstrap/functions/timthumb.php?src=https://st2.depositphotos.com/1699440/8512/i/950/depositphotos_85123736-stock-photo-close-up-pizza-hawai.jpg&w=1023&h=682",
+        priceEst: 130,
         categories: [FoodCategories.ALL],
       },
       {
         name: "Pizza Šunková",
         image: "http://pekarstvijecminek.cz/204-thickbox_default/pizza-sunkova.jpg",
+        priceEst: 120,
         categories: [FoodCategories.ALL],
       },
       {
         name: "Pizza Vegetariana",
         image: "https://pizzafranci.cz/fotky/pizza/pizza-vegetariana.jpg",
+        priceEst: 120,
         categories: [FoodCategories.ALL, FoodCategories.VEGETARIAN, FoodCategories.FISH],
       },
     ],
@@ -58,12 +63,14 @@ const FOODS: FoodInterface[] = [
         name: "Klasický Kebab",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpaPANYtew9iO10gzLjYy-UnV-TSNdQOJwIA&usqp=CAU",
+        priceEst: 100,
         categories: [FoodCategories.ALL],
       },
       {
         name: "Vegetariánský Kebab",
         image:
           "https://bitofthegoodstuff.com/wp-content/uploads/2018/05/Seitan-Doner-Kebab-5-1000.jpg",
+        priceEst: 100,
         categories: [FoodCategories.ALL, FoodCategories.VEGETARIAN, FoodCategories.FISH],
       },
     ],
@@ -77,18 +84,21 @@ const FOODS: FoodInterface[] = [
       {
         name: "Kuře kung-pao",
         image: "https://www.vartesmajklem.cz/picture/large/20190205092304799979724.jpg",
+        priceEst: 100,
         categories: [FoodCategories.ALL],
       },
       {
         name: "Nudle s kuřecím",
         image:
           "https://www.kvalitnitunak.eu/fotky89909/blog/kureci-s-ryzovymi-nudlemi-a-zeleninou.jpeg",
+        priceEst: 100,
         categories: [FoodCategories.ALL],
       },
       {
         name: "Nudle se zeleninou",
         image:
           "https://itesco.cz/imgglobal/content_platform/recipes/main/17/sized/756x426-100-fff-0-0/17c2044f0c8c136f69f4cb95c3dc90ff.jpg",
+        priceEst: 100,
         categories: [FoodCategories.ALL, FoodCategories.VEGETARIAN, FoodCategories.FISH],
       },
     ],
