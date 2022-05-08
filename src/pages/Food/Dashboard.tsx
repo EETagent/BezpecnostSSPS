0877afab2bac633e11415c4bbcc1c08251b3d134
EETagent <vojta.jungmann@gmail.com>
2022-05-08 22:55:10 +0200

    fix: replace or with and

--- src/pages/Food/Dashboard.tsx
+++ src/pages/Food/Dashboard.tsx
@@ -104,7 +104,7 @@ const Dashboard: Component = () => {
       >
         Refresh objednávek
       </button>
-      <Show when={!items.loading || !items.error}>
+      <Show when={!items.loading && !items.error}>
         <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="inline-block py-2 min-w-full sm:px-6 lg:px-8">
             <div class="overflow-hidden shadow-md sm:rounded-lg">
