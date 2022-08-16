66da040c782f3f4c92e05c2c87da7c396f683cdb
EETagent <vojta.jungmann@gmail.com>
2022-08-16 11:10:16 +0200

    feat: zjednodušení db init

--- public/backend/food/db/init.php
+++ public/backend/food/db/init.php
@@ -4,13 +4,11 @@ const DB_PATH = 'food.db';
 
 function init_db($write = false)
 {
-    if (!file_exists(DB_PATH)) {
-        $db = new SQLite3(DB_PATH);
-        $table_create_query = "CREATE TABLE food(token TEXT NOT NULL UNIQUE, email TEXT NOT NULL, name TEXT, day TEXT, food TEXT)";
-        $db->exec($table_create_query);
-        $db->close();
-    }
-
+    $db = new SQLite3(DB_PATH);
+    $table_create_query = "CREATE TABLE IF NOT EXISTS food(token TEXT NOT NULL UNIQUE, email TEXT NOT NULL, name TEXT, day TEXT, food TEXT)";
+    $db->exec($table_create_query);
+    $db->close();
+    
     if ($write === true) {
         $db = new SQLite3(DB_PATH, SQLITE3_OPEN_READWRITE);
     } else {
